a2208ab5b98c4898e9cebbec0031e7e733ec8f68
WilgnerJR <wilgnerbento@gmail.com>
2023-05-06 23:11:07 -0400

    up

--- js/slideModal.js
+++ js/slideModal.js
@@ -1,25 +1,33 @@
-var imgModal = document.getElementById('imgModal');
 
-function slideProduto1() {
-  imgModal.classList.add('simplesmenteAtivo');
+var lista1 = [
+  "https://wilgnerjr.github.io/imagens/imgOtimizada/adesivo.png",
+  "https://wilgnerjr.github.io/imagens/imgOtimizada/banner.webp",
+  "https://wilgnerjr.github.io/imagens/imgOtimizada/blocoDeComanda.webp"];
+var lista2 = [
+  "https://wilgnerjr.github.io/imagens/imgOtimizada/caixaPersonalizada.png",
+  "https://wilgnerjr.github.io/imagens/imgOtimizada/calendario.png",
+  "https://wilgnerjr.github.io/imagens/imgOtimizada/camisa.png"];
 
-  if (imgModal.classList.contains('simplesmenteAtivo')) {
-    var lista = ["https://wilgnerjr.github.io/imagens/imgOtimizada/adesivo.png", "https://wilgnerjr.github.io/imagens/imgOtimizada/banner.webp", "https://wilgnerjr.github.io/imagens/imgOtimizada/blocoDeComanda.webp"];
-    var i = 0;
+var imagemDiv = document.getElementById("imagemDiv");
 
-    while (i < lista.length) {
-      imgModal.style.backgroundImage = lista[i];
-      i++;
-      sleep(5);
-      alert('erro');
-      if( i >= lista.length){
-        i = 0;
-      }
-    }
-  }
-  
+function imprimirLista(lista) {
+  let i = 0;
+  var btn1 = document.getElementById('btn1').addEventListener('click', () => {
+      clearInterval(timer);
+  });
 
+  var btn2 = document.getElementById('btn2').addEventListener('click', () => {
+      clearInterval(timer);
+  });
+
+
+
+
+
+
+  const timer = setInterval(() => {
+
+      imagemDiv.src = lista[i];
+      i = i === lista.length - 1 ? 0 : i + 1;
+  }, 2000);
 }
-
-
-
