eac613a65e24c8f1eba51e143162039e54d68842
WilgnerJR <wilgnerbento@gmail.com>
2023-05-06 21:09:25 -0400

    update

--- js/slideModal.js
+++ js/slideModal.js
@@ -1,17 +1,25 @@
-const div = document.getElementById('imgModal');
-const imagens = [,,]; // lista de imagens
-let i = 0; // índice da imagem atual
-let intervalId = setInterval(trocarImagem, 1000); // armazena o ID retornado por setInterval
+var imgModal = document.getElementById('imgModal');
 
-function trocarImagem() {
-  div.style.backgroundImage = `url(${imagens[i]})`; // atualiza o fundo da div
-  i = (i + 1) % imagens.length; // incrementa o índice e garante o looping infinito
+function slideProduto1() {
+  imgModal.classList.add('simplesmenteAtivo');
+
+  if (imgModal.classList.contains('simplesmenteAtivo')) {
+    var lista = ["https://wilgnerjr.github.io/imagens/imgOtimizada/adesivo.png", "https://wilgnerjr.github.io/imagens/imgOtimizada/banner.webp", "https://wilgnerjr.github.io/imagens/imgOtimizada/blocoDeComanda.webp"];
+    var i = 0;
+
+    while (i < lista.length) {
+      imgModal.style.backgroundImage = lista[i];
+      i++;
+      sleep(5);
+      alert('erro');
+      if( i >= lista.length){
+        i = 0;
+      }
+    }
+  }
+  
+
 }
 
-function pararTrocaImagem() {
-  clearInterval(intervalId); // para a execução da função
-}
 
-// Exemplo de uso:
-// Para parar a execução da função depois de 10 segundos
-// setTimeout(pararTrocaImagem, 1000);
+
